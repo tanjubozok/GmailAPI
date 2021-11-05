3b5085dbd5fb4429febb660f76a5294dd4fccb3e
Tanju BOZOK <tanjubozok@hotmail.com>
2021-11-05 11:41:04 +0400

    Add project files.

---
+++ GmailAPI/APIHelper/Gmail.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GmailAPI.APIHelper
+{
+    public class Gmail
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Body { get; set; }
+        public DateTime MailDateTime { get; set; }
+        public List<string> Attachments { get; set; }
+        public string MsgID { get; set; }
+    }
+}
